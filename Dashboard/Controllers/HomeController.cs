033e6d96cdbe3fc3845ba906d52e39a30ff4613f
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-10-11 07:07:36 -0400

    restarting glm

--- Dashboard/Controllers/HomeController.cs
+++ Dashboard/Controllers/HomeController.cs
@@ -1,34 +1,25 @@
-﻿using Dashboard.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Dashboard.Controllers {
     public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
-        }
-
-        public IActionResult Index() {
-            return View();
-        }
-        public IActionResult Dashboard() {
+        public ActionResult Index() {
             return View();
         }
 
-        public IActionResult Privacy() {
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
         }
     }
 }
