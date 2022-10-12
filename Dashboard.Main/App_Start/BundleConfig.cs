eef89760bbceeb670770c6571ced457f28966db5
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-10-12 19:10:20 -0400

    fixed an issue with libman

--- Dashboard.Main/App_Start/BundleConfig.cs
+++ Dashboard.Main/App_Start/BundleConfig.cs
@@ -1,9 +1,8 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Dashboard.Main {
     public class BundleConfig {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new Bundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
@@ -11,8 +10,6 @@ namespace Dashboard.Main {
             bundles.Add(new Bundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new Bundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
