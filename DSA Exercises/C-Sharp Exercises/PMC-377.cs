aa98c7dcaf315912740b0d18ee685ad1007512dc
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-01-21 08:03:36 -0500

    checking adj ele an arr

--- DSA Exercises/C-Sharp Exercises/PMC-377.cs
+++ DSA Exercises/C-Sharp Exercises/PMC-377.cs
@@ -5,6 +5,35 @@
 
 
 
+// DSAP-06 Extra: Exercise - 55 Basic Declaration and Algorithm
+// Function to calculate the maximum product of adjacent elements in an array
+    public static int array_adjacent_elements_product(int[] input_array)
+    {
+        // Initializing variables
+        int array_index = 0;
+
+        // Calculating the product of the first two elements in the array
+        int product = input_array[array_index] * input_array[array_index + 1];
+
+        // Moving to the next element in the array
+        array_index++;
+
+        // Loop to calculate the maximum product of adjacent elements
+        while (array_index + 1 < input_array.Length)
+        {
+            // Checking if the product of the current adjacent elements is greater than the existing product
+            product = ((input_array[array_index] * input_array[array_index + 1]) > product) ?
+                       (input_array[array_index] * input_array[array_index + 1]) :
+                        product;
+
+            // Moving to the next pair of adjacent elements in the array
+            array_index++;
+        }
+
+        // Returning the maximum product of adjacent elements in the array
+        return product;
+    }
+
 // DSAP-07 Extra: Exercise - 28 Basic Declaration and Algorithm
 // Reverse the words of a sentence
 // // Method to reverse the entire sentence around.
