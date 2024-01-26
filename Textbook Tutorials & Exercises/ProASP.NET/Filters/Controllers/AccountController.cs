f39a7e29b8e1aa19b8059a916b941ae030b9c901
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-01-26 07:50:48 -0500

    starting the filters ch

---
+++ Textbook Tutorials & Exercises/ProASP.NET/Filters/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Filters.Controllers {
+    public class AccountController : Controller {
+        // GET: Account
+        public ActionResult Login() {
+            return View();
+        }
+
+        [HttpPost] [Obsolete]
+        public ActionResult Login(string username, string password, string returnUrl) {
+            bool result = FormsAuthentication.Authenticate(username, password);
+            if(result) {
+                FormsAuthentication.SetAuthCookie(username, false);
+                return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+            } else {
+                ModelState.AddModelError("", "Icorrect Username or password");
+                return View();
+            }
+        }
+    }
+}
---
+++ Textbook Tutorials & Exercises/ProASP.NET/Filters/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public string Index() {
+            return "This is the index action on the home controller";
+        }
+    }
+}
