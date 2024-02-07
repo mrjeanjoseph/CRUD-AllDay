b42b2eaebcdb6bf20b7f1b75fff35053ba7a1ab4
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-02-07 06:20:22 -0500

    get started with mod opt and locking

--- Pluralsight Tutorials/2. Developing SQL Server Databases/5. Managing Database Concurrency/06. Locking in the SQL Server Database Engine/LockEscalation.sql
+++ Pluralsight Tutorials/2. Developing SQL Server Databases/5. Managing Database Concurrency/06. Locking in the SQL Server Database Engine/LockEscalation.sql
@@ -8,7 +8,7 @@ BEGIN TRAN;
 
 INSERT INTO orders.OrderItems
     (OrderID, OrderYear, StockID, Quantity, Discount)
-SELECT TOP (100) v.*
+SELECT TOP (10000) v.*
     FROM (VALUES (1, 2019, 1, 1, 0)) v(a,b,c,d,e)
  CROSS JOIN sys.columns c1
  CROSS JOIN sys.columns c2;
