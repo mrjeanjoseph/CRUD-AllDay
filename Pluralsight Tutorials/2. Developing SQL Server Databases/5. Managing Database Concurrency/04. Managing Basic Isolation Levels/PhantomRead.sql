ba3e7cc821d54290c50b53e2985d15c263dc0148
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-02-06 20:06:27 -0500

    started the locking section mod

--- Pluralsight Tutorials/2. Developing SQL Server Databases/5. Managing Database Concurrency/04. Managing Basic Isolation Levels/PhantomRead.sql
+++ Pluralsight Tutorials/2. Developing SQL Server Databases/5. Managing Database Concurrency/04. Managing Basic Isolation Levels/PhantomRead.sql
@@ -5,6 +5,9 @@ BEGIN TRAN;
 
 INSERT INTO Orders.Orders (
   OrderYear, OrderDate, OrderRequestedDate, CustID)
-    VALUES (2019, '20190701', '20190808', 2);
+    VALUES 
+		(2019, '20190701', '20190808', 2),
+		(2019, '20190701', '20190808', 2),
+		(2019, '20190701', '20190808', 2);
 
 ROLLBACK;    
--- Pluralsight Tutorials/2. Developing SQL Server Databases/5. Managing Database Concurrency/05. Implementing Snapshot Isolation Levels/ReadCommitted.sql
+++ Pluralsight Tutorials/2. Developing SQL Server Databases/5. Managing Database Concurrency/05. Implementing Snapshot Isolation Levels/ReadCommitted.sql
@@ -4,9 +4,9 @@ GO
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 
 BEGIN TRAN;
-
+    -- Before updating
     SELECT * FROM Orders.Orders;
 
+    -- After the update
     SELECT * FROM Orders.Orders;
-
 COMMIT;
