d0b1e0cae7dd3283d32f01a29d4582ba95bc0ebc
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-02-14 07:17:19 -0500

    fixed issue with controller factory

--- Textbook Exercises/ProASP.NET/ControllerExtensibility/Controllers/CustomerController.cs
+++ Textbook Exercises/ProASP.NET/ControllerExtensibility/Controllers/CustomerController.cs
@@ -4,11 +4,11 @@ using System.Web.Mvc;
 namespace ControllerExtensibility.Controllers {
     public class CustomerController : Controller {
         public ViewResult Index() {
-            return View("Result", new Result { ControllerName = "Product", ActionName = "Index" });
+            return View("Result", new Result { ControllerName = "Customer", ActionName = "Index" });
         }
 
         public ViewResult List() {
-            return View("Result", new Result { ControllerName = "Product", ActionName = "List" });
+            return View("Result", new Result { ControllerName = "Customer", ActionName = "List" });
         }
     }
 }
--- Textbook Exercises/ProASP.NET/ControllerExtensibility/Controllers/ProductController.cs
+++ Textbook Exercises/ProASP.NET/ControllerExtensibility/Controllers/ProductController.cs
@@ -3,7 +3,18 @@ using System.Web.Mvc;
 
 namespace ControllerExtensibility.Controllers {
     public class ProductController : Controller {
-        // GET: Product
+        public ViewResult Index() {
+            return View("Result", new Result {
+                ControllerName = "Product",
+                ActionName = "Index"
+            });
+        }
 
+        public ViewResult List() { 
+            return View("Result", new Result { 
+                ControllerName = "Product", 
+                ActionName = "List" 
+            }); 
+        }
     }
 }
