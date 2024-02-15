26078fbfdde4f72429d7713f6e750c0bd2ba42d5
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-02-15 07:16:59 -0500

    Async methods in controllers

--- Textbook Exercises/ProASP.NET/ControllerExtensibility/Controllers/RemoteDataController.cs
+++ Textbook Exercises/ProASP.NET/ControllerExtensibility/Controllers/RemoteDataController.cs
@@ -1,12 +1,26 @@
 ﻿using ControllerExtensibility.Models;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace ControllerExtensibility.Controllers {
     public class RemoteDataController : Controller {
-        public ActionResult Data() {
+        public ActionResult DataOld() {
             RemoteService service = new RemoteService();
             string data = service.GetRemoteData();
             return View((object)data);
+        }
+
+        public async Task<ActionResult> Data() {
+            string data = await Task<string>.Factory.StartNew(() => {
+                return new RemoteService().GetRemoteData();
+            });
+
+            return View((object)data);
+        }
+
+        public async Task<ActionResult> ConsumeAsyncMethod() {
+            string data = await new RemoteService().GetRemoteDataAsync();
+            return View("Data", (object)data);
         }
     }
 }
--- Textbook Exercises/ProASP.NET/ControllerExtensibility/Models/RemoteService.cs
+++ Textbook Exercises/ProASP.NET/ControllerExtensibility/Models/RemoteService.cs
@@ -1,10 +1,18 @@
 ﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace ControllerExtensibility.Models {
     public class RemoteService {
         public string GetRemoteData() {
             Thread.Sleep(2000);
             return "Hello from the other side of the world";
+        }
+
+        public async Task<string> GetRemoteDataAsync() {
+            return await Task<string>.Factory.StartNew(() => {
+                Thread.Sleep(2000);
+                return "Hello from Async and Await";
+            });
         }
     }
 }
