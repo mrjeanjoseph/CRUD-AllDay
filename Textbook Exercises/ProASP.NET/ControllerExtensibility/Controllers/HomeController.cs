c7a8521803a91eacc5e57989e0046751c97912a5
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-02-14 20:08:35 -0500

    action method selector

---
+++ Textbook Exercises/ProASP.NET/ControllerExtensibility/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using ControllerExtensibility.Infrastructure;
+using ControllerExtensibility.Models;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        //public ActionResult Index() {
+        //    return View("Result", new Result {
+        //        ControllerName = "Home",
+        //        ActionName = "Index"
+        //    });
+        //}
+
+        //[Local, ActionName("Index")]
+        //public ActionResult LocalIndex() {
+        //    return View("Result", new Result {
+        //        ControllerName = "Home",
+        //        ActionName = "Index"
+        //    });
+        //}
+
+        protected override void HandleUnknownAction(string actionName) {
+            Response.Write(string.Format("You requested the {0} action", actionName));
+        }
+    }
+}
