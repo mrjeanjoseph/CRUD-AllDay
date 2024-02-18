2fc9c1aabc7b32f6a152838469be012f0e58bb94
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-02-18 12:31:54 -0500

    render all the views

---
+++ Textbook Exercises/ProASP.NET/WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public ActionResult Index() {
+
+            string[] names = { "Apples", "Oranges", "Mango", "Guayava", "Passion Fruit" };
+            return View(names);
+        }
+    }
+}
