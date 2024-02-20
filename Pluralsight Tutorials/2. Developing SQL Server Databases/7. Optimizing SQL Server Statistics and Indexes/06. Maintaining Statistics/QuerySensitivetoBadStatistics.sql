80857f5815ba18bb1f199958ae1be98f16f6ddf2
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-02-20 07:50:33 -0500

    updated statistics

--- Pluralsight Tutorials/2. Developing SQL Server Databases/7. Optimizing SQL Server Statistics and Indexes/06. Maintaining Statistics/QuerySensitivetoBadStatistics.sql
+++ Pluralsight Tutorials/2. Developing SQL Server Databases/7. Optimizing SQL Server Statistics and Indexes/06. Maintaining Statistics/QuerySensitivetoBadStatistics.sql
@@ -19,22 +19,22 @@ WHERE TransactionDate > '2451-01-02'
 
 
 /*
-(15298 rows affected)
-Table 'Workfile'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
-Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
-Table 'Shipments'. Scan count 1, logical reads 254, physical reads 1, read-ahead reads 252, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
-Table 'Transactions'. Scan count 1, logical reads 166, physical reads 1, read-ahead reads 164, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+	(15298 rows affected)
+	Table 'Workfile'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+	Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+	Table 'Shipments'. Scan count 1, logical reads 254, physical reads 1, read-ahead reads 252, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+	Table 'Transactions'. Scan count 1, logical reads 166, physical reads 1, read-ahead reads 164, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
 
- SQL Server Execution Times:
-   CPU time = 46 ms,  elapsed time = 261 ms.
+	 SQL Server Execution Times:
+	   CPU time = 46 ms,  elapsed time = 261 ms.
 */
 
 /*
-Table 'Shipments'. Scan count 0, logical reads 13260, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
-Table 'Transactions'. Scan count 1, logical reads 13278, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+	Table 'Shipments'. Scan count 0, logical reads 13260, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+	Table 'Transactions'. Scan count 1, logical reads 13278, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
 
-(1 row affected)
+	(1 row affected)
 
- SQL Server Execution Times:
-   CPU time = 47 ms,  elapsed time = 119 ms.
+	 SQL Server Execution Times:
+	   CPU time = 47 ms,  elapsed time = 119 ms.
 */
--- Pluralsight Tutorials/2. Developing SQL Server Databases/7. Optimizing SQL Server Statistics and Indexes/06. Maintaining Statistics/ShowStatistics.sql
+++ Pluralsight Tutorials/2. Developing SQL Server Databases/7. Optimizing SQL Server Statistics and Indexes/06. Maintaining Statistics/ShowStatistics.sql
@@ -1,3 +1,14 @@
-DBCC SHOW_STATISTICS(Transactions, 'idx_Transactions_TransactionDate')
+USE [InterstellarTransport];
+GO
 
+--Create this Index first
+CREATE NONCLUSTERED INDEX [idx_Transactions_TransactionDate]
+	ON [dbo].[Transactions] ([TransactionDate])
+	INCLUDE ([ReferenceShipmentID],[TransactionType],[Amount],[InvoiceNumber])
+GO
 
+DBCC SHOW_STATISTICS(Transactions, 'idx_Transactions_TransactionDate');
+GO
+
+UPDATE STATISTICS dbo.Transactions idx_Transaction_TransactionDate WITH FULLSCAN;
+GO
