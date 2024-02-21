be60b132a48f237be9e01177c188fa14efa55fa9
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-02-21 07:33:45 -0500

    helper methods

---
+++ Textbook Exercises/ProASP.NET/Ch21_HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace Ch21_HelperMethods.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public ActionResult Index() {
+
+            ViewBag.Fruits = new string[] { "Gwayav", "Melon", "Pistash", "Mango Fransik" };
+            ViewBag.Cities = new string[] { "Lonsie", "Rwasek", "Plateau Central", "Delmar 24" };
+
+            string message = "This is an HTML element: <input>";
+            return View((object)message);
+        }
+    }
+}
---
+++ Textbook Exercises/ProASP.NET/Ch21_HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace Ch21_HelperMethods.Infrastructure {
+    public static class CustomHelpers {
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, string[] list) {
+            TagBuilder tag = new TagBuilder("ul");
+
+            foreach(string str in list) {
+                TagBuilder itemTag = new TagBuilder("li");
+                itemTag.SetInnerText(str);
+                tag.InnerHtml += itemTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+
+
+        public static MvcHtmlString DisplayMessage(this HtmlHelper html, string message) {
+            string result = string.Format("This is the message from HTML Helper: <p>{0}</p>", message);
+            return new MvcHtmlString(result);
+        }
+    }
+}
