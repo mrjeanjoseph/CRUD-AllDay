e9a6d8d6c7e18a6f59a24a7c4ac3f45a12664554
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-02-21 19:48:45 -0500

    input element from model property

--- Textbook Exercises/ProASP.NET/Ch21_HelperMethods/App_Start/RouteConfig.cs
+++ Textbook Exercises/ProASP.NET/Ch21_HelperMethods/App_Start/RouteConfig.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Ch21_HelperMethods
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
+namespace Ch21_HelperMethods {
+    public class RouteConfig {
+        public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "FormRoute",
+                url: "app/forms/{controller}/{action}"
             );
         }
     }
