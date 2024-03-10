d2d925f2cc5acb4fb234736c9f9cd25bb03519b7
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-03-10 12:10:07 -0400

    setting all the files

--- Textbook Exercises/ProASP.NET/Ch24_MvcModels/Controllers/HomeController.cs
+++ Textbook Exercises/ProASP.NET/Ch24_MvcModels/Controllers/HomeController.cs
@@ -1,10 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using Ch24_MvcModels.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Ch24_MvcModels.Controllers {
     public class HomeController : Controller {
+
+
+        private readonly Person[] personData = {
+            new Person {FirstName = "Louna", LastName = "Jean-Joseph", Role = Role.Admin },
+            new Person {FirstName = "Raoul H.", LastName = "Jean-Joseph", Role = Role.Admin },
+            new Person {FirstName = "Jovenel", LastName = "Moise", Role = Role.User },
+            new Person {FirstName = "Eurie", LastName = "Latortue", Role = Role.User },
+            new Person {FirstName = "Denver", LastName = "Mosque", Role = Role.Guest }
+        };
+
+
         // GET: Home
-        public ActionResult Index() {
-            return View();
+        public ActionResult Index(int id) {
+            Person personItem = personData.Where(p => p.PersonId == id).First();
+            return View(personItem);
         }
     }
 }
