d4047c82ce86ba7571d71d542c765fc880f144f6
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-03-15 07:38:07 -0400

    created the layout

---
+++ Textbook Exercises/ProASP.NET/Ch25_ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Ch25_ModelValidation.Models;
+using System;
+using System.Web.Mvc;
+
+namespace Ch25_ModelValidation.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+
+        public ViewResult MakeBooking() {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt) {
+            // statement to store new appointment in a
+            // repository would go here in a real project
+
+            return View("Completed", appt);
+        }
+    }
+}
