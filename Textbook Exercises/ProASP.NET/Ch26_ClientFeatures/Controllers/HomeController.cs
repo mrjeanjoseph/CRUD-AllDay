b1cac952836c19cc2e18d9e0ac360b6cbaebdff4
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-03-15 18:40:29 -0400

    creating the model and contr

---
+++ Textbook Exercises/ProASP.NET/Ch26_ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Ch26_ClientFeatures.Models;
+using System.Web.Mvc;
+
+namespace Ch26_ClientFeatures.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public ActionResult MakeBooking() {
+            return View(new Appointment {
+                ClientName = "Jovenel",
+                TermsAccepted = true
+            });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appointment) {
+            return Json(appointment, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
