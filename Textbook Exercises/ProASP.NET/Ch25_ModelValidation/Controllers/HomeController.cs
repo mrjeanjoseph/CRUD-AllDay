d08912d1049956f2f922236c2c3a6c51f4935512
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-03-15 13:47:20 -0400

    using alternative validation techniques

--- Textbook Exercises/ProASP.NET/Ch25_ModelValidation/Controllers/HomeController.cs
+++ Textbook Exercises/ProASP.NET/Ch25_ModelValidation/Controllers/HomeController.cs
@@ -16,21 +16,21 @@ namespace Ch25_ModelValidation.Controllers {
         [HttpPost]
         public ViewResult MakeBooking(Appointment appt) {
 
-            if (string.IsNullOrEmpty(appt.ClientName))
-                ModelState.AddModelError("ClientName", "Please enter your name");
+            //if (string.IsNullOrEmpty(appt.ClientName))
+            //    ModelState.AddModelError("ClientName", "Please enter your name");
 
-            if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date)
-                ModelState.AddModelError("Date", "Please enter a date in the future");
+            //if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date)
+            //    ModelState.AddModelError("Date", "Please enter a date in the future");
 
-            if (!appt.TermsAccepted)
-                ModelState.AddModelError("TermsAccepted", "You must accept the terms");
+            //if (!appt.TermsAccepted)
+            //    ModelState.AddModelError("TermsAccepted", "You must accept the terms");
 
-            if (ModelState.IsValidField("ClientName") && 
-                ModelState.IsValidField("Date") && 
-                appt.ClientName == "Ariel" && 
-                appt.Date.DayOfWeek == DayOfWeek.Monday) {
-                ModelState.AddModelError("", "Ariel Henry is unable to setup an appointment for right now.");
-            }
+            //if (ModelState.IsValidField("ClientName") && 
+            //    ModelState.IsValidField("Date") && 
+            //    appt.ClientName == "Ariel" && 
+            //    appt.Date.DayOfWeek == DayOfWeek.Monday) {
+            //    ModelState.AddModelError("", "Ariel Henry is unable to setup an appointment for right now.");
+            //}
 
             if (ModelState.IsValid)
                 return View("Completed", appt);
---
+++ Textbook Exercises/ProASP.NET/Ch25_ModelValidation/Infrastructure/MustBeTrueAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ch25_ModelValidation.Infrastructure {
+    public class MustBeTrueAttribute :ValidationAttribute {
+        //public override bool IsValid(object value) => value is bool v && v;
+
+        public override bool IsValid(object value) {
+            //return value is bool && (bool)value;
+
+            return value is bool v && v;
+        }
+    }
+}
