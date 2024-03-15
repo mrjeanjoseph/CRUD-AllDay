8dc3ac3e8e5822f33e7e7f48d17f1f83b8ab6442
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-03-15 18:02:23 -0400

    added remote validation

--- Textbook Exercises/ProASP.NET/Ch25_ModelValidation/Models/Appointment.cs
+++ Textbook Exercises/ProASP.NET/Ch25_ModelValidation/Models/Appointment.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Ch25_ModelValidation.Models {
 
     public class Appointment {
 
-        [Required, StringLength(10,MinimumLength =3)]
+        [Required, StringLength(10, MinimumLength = 3)]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
+        [Remote("ValidateDate", "Home")]
         public DateTime Date { get; set; }
 
         public bool TermsAccepted { get; set; }
