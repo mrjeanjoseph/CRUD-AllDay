91259b6d215d1606aa67d196467c029bb7b0e7e7
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-03-17 11:08:21 -0400

    rem unecessary files and folders

--- Textbook Exercises/ProASP.NET/Ch27_WebServices/App_Start/RouteConfig.cs
+++ Textbook Exercises/ProASP.NET/Ch27_WebServices/App_Start/RouteConfig.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Ch27_WebServices
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
+namespace Ch27_WebServices {
+    public class RouteConfig {
+        public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
--- Textbook Exercises/ProASP.NET/Ch27_WebServices/App_Start/WebApiConfig.cs
+++ Textbook Exercises/ProASP.NET/Ch27_WebServices/App_Start/WebApiConfig.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
-namespace Ch27_WebServices
-{
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+namespace Ch27_WebServices {
+    public static class WebApiConfig {
+        public static void Register(HttpConfiguration config) {
 
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
