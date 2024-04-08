ff3ea01005ba4fc17bba8073f8b4fa055287326e
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-04-08 05:38:09 -0400

    man the files to what we want

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/Chapter2.FirstApp.MVC/Areas/Chapter1/Chapter1AreaRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Chapter2.FirstApp.MVC.Areas.Chapter1 {
+    public class Chapter1AreaRegistration : AreaRegistration {
+        public override string AreaName {
+            get {
+                return "Chapter1";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) {
+            context.MapRoute(
+                "Chapter1",
+                "Chapter1/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Textbook Exercises/PRO_ASP.NET_MVC/Chapter2.FirstApp.MVC/Areas/Chapter1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chapter2.FirstApp.MVC.Areas.Chapter1.Controllers {
+    public class HomeController : Controller {
+        // GET: Chapter1/Home
+        public string Index() {
+            return "<h1>Hello ASP.NET MVC 5</h1> - \nThis is Chapter 1, the introduction";
+        }
+    }
+}
