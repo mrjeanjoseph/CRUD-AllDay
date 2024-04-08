951fbb517cb68ce37e28536a75a695aaa263d38f
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-04-08 19:54:48 -0400

    files for ch3 added to normal

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartOne/Areas/Chapter3/Models/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Chapter2.TheMVCPattern.Models {
+    public interface IUserRepository {
+        void Add(User user);
+
+        User FetchByLoginName(string loginName);
+
+        void SubmitChanges();
+    }
+}
