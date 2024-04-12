dc0b89a59b35f5d65e0ccbcd47d0ccc026eb8a8e
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-04-11 20:00:06 -0400

    covering razor syntaxes

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartOne/Areas/Chapter5/Chapter5AreaRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace WorkingWithRazor {
+    public class Chapter5AreaRegistration : AreaRegistration {
+        public override string AreaName {
+            get {
+                return "Chapter5";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) {
+            context.MapRoute(
+                "WorkingWithRazor",
+                "Chapter5/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartOne/Areas/Chapter5/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using WorkingWithRazor.Models;
+
+namespace WorkingWithRazor.Controllers {
+    public class HomeController : Controller {
+        readonly Product createProduct = new Product {
+            ProductID = 101,
+            Name = "Bouret",
+            Description = "Yon gros bouret",
+            Category = "Biznis Pesonel",
+            Price = 5725M,
+            IsAvailable = true
+        };
+        
+        // GET: Chapter5/Home
+        public ActionResult Index() {
+            return View(createProduct);
+        }
+    }
+}
