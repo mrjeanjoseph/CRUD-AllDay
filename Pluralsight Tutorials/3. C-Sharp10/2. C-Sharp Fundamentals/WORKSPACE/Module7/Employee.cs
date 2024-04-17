3eaa936f89f36ae42347dc3f7bf5308cc735a9a6
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-04-17 04:59:09 -0400

    covered classes and objects

--- Pluralsight Tutorials/3. C-Sharp10/2. C-Sharp Fundamentals/WORKSPACE/Module7/Employee.cs
+++ Pluralsight Tutorials/3. C-Sharp10/2. C-Sharp Fundamentals/WORKSPACE/Module7/Employee.cs
@@ -1,1 +1,52 @@
-﻿
+﻿namespace ClassesAndObjects {
+    internal class Employee {
+        public string firstName;
+        public string lastName;
+
+        public string emailAddress;
+
+        public int numberOfHoursWorked;
+        public double standardWage;
+        public double hourlyRate;
+        const int minimumHrsWorkedUnit = 1;
+
+        public DateTime birthDate;
+
+        //Custom constructors
+        public Employee(string first, string last, string em, DateTime bd) 
+            : this(first, last, em, bd, 0) { }
+        public Employee(string first, string last, string em, DateTime bd, double rate) {
+            firstName = first;
+            lastName = last;
+            emailAddress = em;
+            birthDate = bd;
+            hourlyRate = rate;
+        }
+
+        public void PerformWork() {
+            //numberOfHoursWorked++;
+            PerformWork(minimumHrsWorkedUnit);
+            //Console.WriteLine($"{firstName} {lastName} worked for {numberOfHoursWorked} hours.");
+        }
+        public void PerformWork(int numberOfHours) {
+            numberOfHoursWorked += numberOfHours;
+            Console.WriteLine($"{firstName} {lastName} worked for {numberOfHours} hours.");
+        }
+
+        //Employee can receive wages
+        public double ReceiveWage(bool resetHours = true) {
+            standardWage = numberOfHoursWorked * hourlyRate;
+            Console.WriteLine($"{firstName} {lastName} has recieved a wage of {standardWage} for {numberOfHoursWorked} hours worked.");
+
+            if (resetHours) {
+                numberOfHoursWorked = 0;
+            }
+
+            return standardWage;
+        }
+
+        public void DisplayEmployeeDetails() {
+            Console.WriteLine($"\nFirst Name: \t{firstName} \nLast Name: \t{lastName} \nEmail Address: \t{emailAddress} \nBirthDay: \t{birthDate.ToShortDateString()}\n");
+        }
+    }
+}
--- Pluralsight Tutorials/3. C-Sharp10/2. C-Sharp Fundamentals/WORKSPACE/Module7/Program.cs
+++ Pluralsight Tutorials/3. C-Sharp10/2. C-Sharp Fundamentals/WORKSPACE/Module7/Program.cs
@@ -1,9 +1,57 @@
-﻿namespace ClassesAndObjects {
+﻿using ClassesAndObjects;
+
+namespace ClassesAndObjects {
     internal class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Creating an employee");
+            Console.WriteLine("Creating employee one:");
             Console.WriteLine("--------------------\n");
+
+            Employee employeeOne = new Employee("Kervens", "Jean-Joseph", "kjeanjoseph@dvc.com", new DateTime(1995, 1,3), 55);
+
+            employeeOne.DisplayEmployeeDetails();
+            employeeOne.PerformWork();
+            employeeOne.PerformWork();
+            employeeOne.PerformWork(7);
+            employeeOne.PerformWork();
+
+            double recievedWageEmployeeOne = employeeOne.ReceiveWage(true);
+            Console.WriteLine($"Wage paid (Message from Program): {recievedWageEmployeeOne}");
+
+            //Creating a new employee
+            Console.WriteLine("\n\nCreating employee two");
+            Console.WriteLine("--------------------\n");
+
+            Employee employeeTwo = new Employee("Julien", "Dure", "jdure01@dvc.com", new DateTime(2009, 12, 13), 48);
+
+            employeeTwo.DisplayEmployeeDetails();
+            employeeTwo.PerformWork();
+            employeeTwo.PerformWork();
+            employeeTwo.PerformWork(7);
+            employeeTwo.PerformWork();
+
+            double recievedWageEmployeeTwo = employeeTwo.ReceiveWage(true);
+            Console.WriteLine($"Wage paid (Message from Program): {recievedWageEmployeeTwo}");
+
+
+            Console.Clear();
+            //Creating a third employee - Manupilate
+            Console.WriteLine("\n\nManupilating existing employee");
+            Console.WriteLine("--------------------\n");
+
+            employeeTwo.firstName = "Elanie";
+            employeeTwo.lastName = "Jean Joseph";
+            employeeTwo.standardWage = 51;
+
+            employeeTwo.DisplayEmployeeDetails();
+            employeeTwo.PerformWork();
+            employeeTwo.PerformWork();
+            employeeTwo.PerformWork(7);
+            employeeTwo.PerformWork();
+
+            double recievedWageEmployeeUpdated = employeeTwo.ReceiveWage(true);
+            Console.WriteLine($"Wage paid (Message from Program): {recievedWageEmployeeUpdated}");
+
 
             Console.ReadLine();
         }
