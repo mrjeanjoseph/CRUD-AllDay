bf40c8da885b73e84fe2ece8f1c64b0168133350
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-04-18 19:16:32 -0400

    coving unit test using moq

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartOne.Tests/Chapter6.Tests/EssentialTool.Test.cs
@@ -0,0 +1,69 @@
+﻿using EssentialTools.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PartOne.Tests {
+    [TestClass]
+    public class EssentialTools {
+
+        private IDiscountCalculator GetTestOject() {
+            return new MinimumDiscountHelper();
+        }
+
+        [TestMethod]
+        public void DiscountAbove100() {
+            //Arrange
+            IDiscountCalculator target = GetTestOject();
+            decimal total = 200;
+
+            //act
+            var discountedTotal = target.ApplyDiscount(total);
+
+            //assert
+            Assert.AreEqual(total * 0.9M, discountedTotal);
+        }
+
+        [TestMethod]
+        public void DiscountBetweenTenAnd100() {
+            //Arrange
+            IDiscountCalculator target = GetTestOject();
+            decimal[] total = { 10, 100, 50 };
+
+            //act
+            var TenDollarDiscount = target.ApplyDiscount(total[0]);
+            var OneHundDollarDiscount = target.ApplyDiscount(total[1]);
+            var FiftyDollarDiscount = target.ApplyDiscount(total[2]);
+
+            //assert
+            Assert.AreEqual(5 * TenDollarDiscount, "$10 discount is wrong");
+            Assert.AreEqual(95 * OneHundDollarDiscount, "$100 discount is wrong");
+            Assert.AreEqual(45 * FiftyDollarDiscount, "$50 discount is wrong");
+        }
+
+        [TestMethod]
+        public void DiscountLessThanTen() {
+            //Arrange
+            IDiscountCalculator target = GetTestOject();
+            decimal[] total = { 9, 5, 0 };
+
+            //act
+            var NineDiscount = target.ApplyDiscount(total[0]);
+            var OneDiscount = target.ApplyDiscount(total[1]);
+            var ZeroDiscount = target.ApplyDiscount(total[2]);
+
+            //assert
+            Assert.AreEqual(5, NineDiscount);
+            Assert.AreEqual(0, OneDiscount);
+            Assert.AreEqual(0, ZeroDiscount);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DiscountNegativeTotal() {
+            //Arrange
+            IDiscountCalculator target = GetTestOject();
+
+            //act
+            target.ApplyDiscount(-1);
+        }
+    }
+}
---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartOne.Tests/Chapter6.Tests/LinqValueCalc.Test.cs
@@ -0,0 +1,33 @@
+﻿using EssentialTools.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace PartOne.Tests {
+
+    [TestClass]
+    public class LinqValueCalc {
+
+        private readonly Merchandise[] merch = {
+            new Merchandise {Name = "Bouret", Category = "Biznis Nasyonal", Price = 1050},
+            new Merchandise {Name = "Barik Pistash", Category = "Biznis Lokal", Price = 3800},
+            new Merchandise {Name = "Pye Palmis", Category = "Biznis Lokal", Price = 2000},
+            new Merchandise {Name = "Konbit pou Rekolt", Category = "Biznis Lokal", Price = 500},
+            new Merchandise {Name = "Sevis Transpo", Category = "Biznis Nasyonal", Price = 250},
+        };
+
+        [TestMethod]
+        public void SumProductCorrectly() {
+            //arrange
+            var discounter = new MinimumDiscountHelper();
+            var target = new LinqValueCalculator(discounter);
+            var goalTotal = merch.Sum(m => m.Price);
+
+            //act
+            var result = target.MerchandiseValue(merch);
+
+            //assert
+            Assert.AreEqual(goalTotal, result);
+        }
+    }
+}
