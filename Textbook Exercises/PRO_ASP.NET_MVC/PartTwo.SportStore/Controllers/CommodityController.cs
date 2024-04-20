296928e9a9a3d5656c8947e1406e4b6888b56dee
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-04-20 16:03:28 -0400

    setting up the domal model

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartTwo.SportStore/Controllers/CommodityController.cs
@@ -0,0 +1,16 @@
+﻿using SportStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportStore.Controllers {
+    public class CommodityController : Controller {
+        private readonly ICommodityRepository _repository;
+
+        public CommodityController(ICommodityRepository repository) {
+            _repository = repository;
+        }
+
+        public ActionResult CommodityList() {
+            return View(_repository.Commodities);
+        }
+    }
+}
--- Textbook Exercises/PRO_ASP.NET_MVC/PartTwo.SportStore/Controllers/HomeController.cs
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartTwo.SportStore/Controllers/HomeController.cs
@@ -3,8 +3,8 @@
 namespace IntroductionToMVC.Controllers {
     public class HomeController : Controller {
         // GET: Home
-        public string Index() {
-            return "<h1>Hello ASP.NET MVC 5</h1> - \nThis is Chapter 1, the introduction";
+        public ActionResult Index() {
+            return View();
         }
     }
 }
