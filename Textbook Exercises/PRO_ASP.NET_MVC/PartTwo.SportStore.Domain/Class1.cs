f20a26ef259cbe352d5a1fe304e4f4441f0a0047
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-04-20 11:08:35 -0400

    setting up the project and unit-test

--- Textbook Exercises/PRO_ASP.NET_MVC/PartTwo.SportStore.Domain/Class1.cs
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartTwo.SportStore.Domain/Class1.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PartTwo.SportStore.Domain
-{
-    public class Class1
-    {
+﻿namespace SportStore.Domain {
+    public class Class1 {
     }
 }
