ba71b22d97c513f20f29005fc99d179c452b77b8
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-04-20 18:03:15 -0400

    baypassing the ioc

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/SportsStore.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace SportsStore.Web.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult List() {
+            return View();
+        }
+    }
+}
