4528e56ff9214ef8377dc359c006212867a2867c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-05-09 18:14:32 -0400

    crud oper for admin

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/SportsStore.Web/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using SportsStore.Domain;
+using System.Web.Mvc;
+
+namespace SportsStore.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IMerchandiseRepository _repository;
+
+        public AdminController(IMerchandiseRepository repoParam)
+        {
+            _repository = repoParam;
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(_repository.Merchandises);
+        }
+    }
+}
