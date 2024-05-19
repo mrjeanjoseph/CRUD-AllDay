2e3da56cf4793253bcffdc18bb90c6f21f55a3ac
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-05-19 07:06:03 -0400

    setting up ch16 urlsandroute

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartThree.AdvConcepts/Areas/Chapter16/Chapter16AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Chapter16.URLsAndRoutes
+{
+    public class Chapter16AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Chapter16";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "",
+                "URLsAndRoutes/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
