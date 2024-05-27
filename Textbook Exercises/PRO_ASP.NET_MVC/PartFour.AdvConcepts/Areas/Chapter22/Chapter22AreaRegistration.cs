3c5e3b3172c54ce93e6b5b42b0758c7adc6f2adb
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-05-27 16:24:06 -0400

    setting up the templated helper method module

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Areas/Chapter22/Chapter22AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Chapter22.TemplatedHelperMethods
+{
+    public class Chapter22AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Chapter22";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "",
+                "TemplatedHelperMethods/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Areas/Chapter22/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Chapter22.TemplatedHelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
