30537f9fad498690a409bdfa9d51616eda6ebc62
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-05-27 10:09:52 -0400

    cleaned up namespaces

--- Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/App_Start/RouteConfig.cs
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/App_Start/RouteConfig.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace PartFour.AdvConcepts
+namespace AdvConcepts
 {
     public class RouteConfig
     {
@@ -16,7 +12,8 @@ namespace PartFour.AdvConcepts
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new string[] { "AdvConcepts.Controllers" }
             );
         }
     }
--- Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Global.asax.cs
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Global.asax.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace PartFour.AdvConcepts
+namespace AdvConcepts
 {
     public class MvcApplication : System.Web.HttpApplication
     {
