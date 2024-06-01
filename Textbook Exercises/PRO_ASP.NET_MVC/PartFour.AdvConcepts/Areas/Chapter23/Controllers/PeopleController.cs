2a75d60adeedc733426af06eb18bc15f4bf59b4f
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-06-01 17:15:51 -0400

    bugs

--- Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Areas/Chapter23/Controllers/PeopleController.cs
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Areas/Chapter23/Controllers/PeopleController.cs
@@ -1,5 +1,7 @@
 ﻿using Chapter23.AjaxHelperMethods.Models;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -15,19 +17,77 @@ namespace Chapter23.AjaxHelperMethods.Controllers
             new Person { FirstName = "Jean-Jacques", LastName = "LeBlanc", Role = Role.Guest },
             new Person { FirstName = "Denzel", LastName = "Dure", Role = Role.User },
         };
-        // GET: Chapter23/People
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult GetPeople()
+        #region Using the Json Way
+        private IEnumerable<Person> GetData(string selectedRole)
+        {
+            IEnumerable<Person> data = personData;
+
+            if (selectedRole != "All")
+            {
+                Role selected = (Role)Enum.Parse(typeof(Role), selectedRole);
+                data = personData.Where(p => p.Role == selected);
+            }
+            return data;
+        }
+
+        public JsonResult GetPeopleDataJson(string selectedRole = "All")
+        {
+            //IEnumerable<Person> data = GetData(selectedRole);
+            var data = GetData(selectedRole).Select(p => new
+            {
+                p.FirstName,
+                p.LastName,
+                Role = Enum.GetName(typeof(Role), p.Role)
+            });
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public PartialViewResult GetPeopleDataJsonWay(string selectedRole = "All")
+        {
+            return PartialView("_GetPeopleData", GetData(selectedRole));
+        }
+        #endregion
+
+        #region Fetching record the Ajax way        
+        public PartialViewResult GetPeopleDataAjaxWay(string selectedRole = "All")
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            IEnumerable<Person> data = personData;
+
+            if (selectedRole != "All")
+            {
+                Role selected = (Role)Enum.Parse(typeof(Role), selectedRole);
+                data = personData.Where(p => p.Role == selected);
+            }
+
+            stopwatch.Stop();
+            ViewBag.ServerProcessingTime = stopwatch.ElapsedMilliseconds;
+
+            return PartialView("_GetPeopleData", data);
+        }
+
+        public ActionResult GetPeople(string selectedRole = "All")
+        {
+            return View((object)selectedRole);
+        }
+        #endregion
+
+        #region Regular way of getting people data
+        public ActionResult GetPeopleRegularWay()
         {
             return View(personData);
         }
 
         [HttpPost]
-        public ActionResult GetPeople(string selectedRole)
+        public ActionResult GetPeopleRegularWay(string selectedRole)
         {
             if (selectedRole == null || selectedRole == "All")            
                 return View(personData);            
@@ -37,5 +97,6 @@ namespace Chapter23.AjaxHelperMethods.Controllers
                 return View(personData.Where(p => p.Role == selected));
             }
         }
+        #endregion
     }
 }
