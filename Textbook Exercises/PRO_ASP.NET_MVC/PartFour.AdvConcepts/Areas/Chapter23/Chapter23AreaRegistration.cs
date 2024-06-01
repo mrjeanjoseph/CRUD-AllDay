e9f642bf5a0544a8adb9eb95dc63f6706baff01b
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-06-01 10:02:17 -0400

    setting up ajax helper method module

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Areas/Chapter23/Chapter23AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Chapter23.AjaxHelperMethods
+{
+    public class Chapter23AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Chapter23";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "",
+                "AjaxHelperMethods/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Areas/Chapter23/Controllers/PeopleController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Chapter23.AjaxHelperMethods.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: Chapter23/People
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
