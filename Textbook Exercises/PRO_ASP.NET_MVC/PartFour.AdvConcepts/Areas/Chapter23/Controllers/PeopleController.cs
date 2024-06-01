55a0d9ef561896814b0b91c6d6d542ce9c37420f
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-06-01 10:30:20 -0400

    created layouts and added css

--- Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Areas/Chapter23/Controllers/PeopleController.cs
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Areas/Chapter23/Controllers/PeopleController.cs
@@ -1,13 +1,41 @@
-﻿using System.Web.Mvc;
+﻿using Chapter23.AjaxHelperMethods.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Chapter23.AjaxHelperMethods.Controllers
 {
     public class PeopleController : Controller
     {
+        private Person[] personData =
+        {
+            new Person { FirstName = "Kervens", LastName = "Jean-Joseph", Role = Role.Admin },
+            new Person { FirstName = "Chistine", LastName = "Fleurant", Role = Role.User },
+            new Person { FirstName = "Nitaud", LastName = "Paniague", Role = Role.User },
+            new Person { FirstName = "Jean-Jacques", LastName = "LeBlanc", Role = Role.Guest },
+            new Person { FirstName = "Denzel", LastName = "Dure", Role = Role.User },
+        };
         // GET: Chapter23/People
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetPeople()
+        {
+            return View(personData);
+        }
+
+        [HttpPost]
+        public ActionResult GetPeople(string selectedRole)
+        {
+            if (selectedRole == null || selectedRole == "All")            
+                return View(personData);            
+            else
+            {
+                Role selected = (Role)Enum.Parse(typeof(Role), selectedRole);
+                return View(personData.Where(p => p.Role == selected));
+            }
         }
     }
 }
