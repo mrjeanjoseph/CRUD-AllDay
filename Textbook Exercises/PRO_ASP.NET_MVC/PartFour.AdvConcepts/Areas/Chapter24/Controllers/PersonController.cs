e013a491624d7f4bdec81706c0d27871bc22eea7
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-06-02 07:18:58 -0400

    set up the model binding module

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Areas/Chapter24/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using Chapter24.ModelBinding;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PartFour.AdvConcepts.Areas.Chapter24.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly Person[] personData =
+{
+            new Person { PersonId = 105, FirstName = "Kervens", LastName = "Jean-Joseph", Role = Role.Admin },
+            new Person { PersonId = 105, FirstName = "Chistine", LastName = "Fleurant", Role = Role.User },
+            new Person { PersonId = 105, FirstName = "Nitaud", LastName = "Paniague", Role = Role.User },
+            new Person { PersonId = 105, FirstName = "Jean-Jacques", LastName = "LeBlanc", Role = Role.Guest },
+            new Person { PersonId = 105, FirstName = "Denzel", LastName = "Dure", Role = Role.User },
+        };
+        // GET: Chapter24/Person
+        public ActionResult Index(int id)
+        {
+            Person dataItem = personData.Where(p => p.PersonId == id).First();
+            return View(dataItem);
+        }
+    }
+}
---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Areas/Chapter24/Models/PersonnelMD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chapter24.ModelBinding.Models
+{
+    [MetadataType(typeof(PersonMetaData))]
+    public partial class Person
+    {
+        public int PersonId { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Address HomeAddress { get; set; }
+        public bool IsApproved { get; set; }
+        public Role Role { get; set; }
+        public string Comments { get; set; }
+
+    }
+
+    public enum Role { Admin, User, Contractor, Guest }
+
+    public class Address
+    {
+        public string LineOne { get; set; }
+        public string LineTwo { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
