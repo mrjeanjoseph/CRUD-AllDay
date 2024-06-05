e8631e1b1c4ae2af8c1209d9fb163b088b6be454
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-06-05 04:50:06 -0400

    bundling chapter completed

---
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+
+namespace PartFour.AdvConcepts
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scriptfiles").Include(
+                "~/Scripts/jquery-{version}.min.js",
+                "~/Scripts/jquery.validate.min.js",
+                "~/Scripts/jquery.validate.unobtrusive.min.js",
+                "~/Scripts/jquery.unobtrusive-ajax.min.js"));
+        }
+    }
+}
--- Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Global.asax.cs
+++ Textbook Exercises/PRO_ASP.NET_MVC/PartFour.AdvConcepts/Global.asax.cs
@@ -1,5 +1,6 @@
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using Chapter24.ModelBinding.Infrastructure;
 using Chapter24.ModelBinding.Models;
@@ -12,6 +13,7 @@ namespace PartFour.AdvConcepts
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             //ValueProviderFactories.Factories.Insert(0, new CustomValueProviderFactory());
             //ModelBinders.Binders.Add(typeof(AddressSummary), new AddressSummaryBinder());
