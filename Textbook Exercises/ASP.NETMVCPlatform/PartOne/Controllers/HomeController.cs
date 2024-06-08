90bcb1a4baa7771c98ddf76f8c078c5a40c1f61a
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-06-08 19:52:02 -0400

    Ch1 and Ch2 both completed

---
+++ Textbook Exercises/ASP.NETMVCPlatform/PartOne/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using PartOne.SimpleApp.Models;
+using System.Web.Mvc;
+
+namespace PartOne.SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Color color)
+        {
+            Color? oldColor = Session["color"] as Color?;
+            if (oldColor != null)
+                Votes.ChangeVote(color, (Color)oldColor);
+            else Votes.RecordVote(color);
+
+            ViewBag.SelectedColor = Session["color"] = color;
+            return View();
+        }
+    }
+}
--- Textbook Exercises/ASP.NETMVCPlatform/PartOne/Models/Votes.cs
+++ Textbook Exercises/ASP.NETMVCPlatform/PartOne/Models/Votes.cs
@@ -1,11 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
-namespace PartOne.Models
+namespace PartOne.SimpleApp.Models
 {
+    public enum Color
+    {
+        Red, Green, Yellow, Purple, Blue, Magenta
+    }
     public class Votes
     {
+        private static readonly Dictionary<Color, int> votes = new Dictionary<Color, int>();
+
+        public static void RecordVote(Color color)
+        {
+            votes[color] = votes.ContainsKey(color) ? votes[color] + 1 : 1;
+        }
+
+        public static void ChangeVote(Color newColor, Color oldColor)
+        {
+            if (votes.ContainsKey(oldColor))
+                votes[oldColor]--;
+            RecordVote(newColor);
+        }
+
+        public static int GetVotes(Color color)
+        {
+            return votes.ContainsKey(color) ? votes[color] : 0;
+        }
     }
 }
