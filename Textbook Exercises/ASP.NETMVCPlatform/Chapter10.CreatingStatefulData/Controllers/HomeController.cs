c0ee4001319f2c464ae418ce5c5ef0e75c91343b
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-06-23 19:48:55 -0400

    app state data implementation

--- Textbook Exercises/ASP.NETMVCPlatform/Chapter10.CreatingStatefulData/Controllers/HomeController.cs
+++ Textbook Exercises/ASP.NETMVCPlatform/Chapter10.CreatingStatefulData/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using CreatingStatefulData.Infrastructure;
+using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace CreatingStatefulData.Controllers
@@ -8,8 +10,26 @@ namespace CreatingStatefulData.Controllers
         public ActionResult Index()
         {
             Dictionary<string, object> dataOject = new Dictionary<string, object>();
-            dataOject.Add("Placeholder Property", "Placeholder Value");
+            //dataOject.Add("Placeholder Property", "Placeholder Value");
+            dataOject.Add("Counter", AppStateHelper.Get(AppStateKeys.COUNTER, 0));
+            IDictionary<AppStateKeys, object> stateData = AppStateHelper
+                .GetMulitple(AppStateKeys.LAST_REQUEST_TIME, AppStateKeys.LAST_REQUEST_URL);
+            foreach (AppStateKeys key in stateData.Keys)
+                dataOject.Add(Enum.GetName(typeof(AppStateKeys), key), stateData[key]);
+
             return View(dataOject);
+        }
+
+        public ActionResult Increment()
+        {
+            int currentValue = (int)AppStateHelper.Get(AppStateKeys.COUNTER, 0);
+            AppStateHelper.Set(AppStateKeys.COUNTER, currentValue + 1);
+            AppStateHelper.SetMultiple(new Dictionary<AppStateKeys, object>
+            {
+                {AppStateKeys.LAST_REQUEST_TIME, HttpContext.Timestamp },
+                {AppStateKeys.LAST_REQUEST_URL, Request.RawUrl},
+            });
+            return RedirectToAction("Index");
         }
     }
 }
