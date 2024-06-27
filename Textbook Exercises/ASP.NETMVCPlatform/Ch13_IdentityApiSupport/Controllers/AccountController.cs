e2e19d35334e11063e0cb8746e1a8a78ee0701cf
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-06-27 18:56:21 -0400

    added authentication

---
+++ Textbook Exercises/ASP.NETMVCPlatform/Ch13_IdentityApiSupport/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using IdentityApiSupport.Infrastructure;
+using IdentityApiSupport.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityApiSupport.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Private Actions
+        private IAuthenticationManager AuthManager
+        {
+            get { return HttpContext.GetOwinContext().Authentication; }
+        }
+
+        private AppUserManager UserManager
+        {
+            get { return HttpContext.GetOwinContext().GetUserManager<AppUserManager>(); }
+        }
+        #endregion
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost, AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel details, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await UserManager.FindAsync(details.Name, details.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Invalid name or password");
+                }
+                else
+                {
+                    ClaimsIdentity identity = await UserManager.CreateIdentityAsync(user,
+                        DefaultAuthenticationTypes.ApplicationCookie);
+                    AuthManager.SignOut();
+                    AuthManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = false
+                    }, identity);
+                    return Redirect(returnUrl);
+                }
+            }
+            return View(details);
+        }
+    }
+}
--- Textbook Exercises/ASP.NETMVCPlatform/Ch13_IdentityApiSupport/Models/UserViewModels.cs
+++ Textbook Exercises/ASP.NETMVCPlatform/Ch13_IdentityApiSupport/Models/UserViewModels.cs
@@ -13,4 +13,13 @@ namespace IdentityApiSupport.Models
         [Required]
         public string Password { get; set; }
     }
+
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
 }
