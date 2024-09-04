4a557d7dccfe21956b704ac371d0ce66be1f64a5
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-09-04 19:11:57 -0400

    Model and infrastructure completed

--- SportsStore/SportsStore.WebUI/Models/MeViewModels.cs
+++ SportsStore/SportsStore.WebUI/Models/MeViewModels.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-namespace SportsStore.WebUI.Models {
+﻿namespace SportsStore.WebUI.Models {
     // Models returned by MeController actions.
     public class GetViewModel {
         public string Hometown { get; set; }
