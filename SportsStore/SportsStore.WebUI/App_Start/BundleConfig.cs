159771af1d886685337112885c42c63d08a2a931
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-09-04 17:48:10 -0400

    work in progress for later

--- SportsStore/SportsStore.WebUI/App_Start/BundleConfig.cs
+++ SportsStore/SportsStore.WebUI/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace SportsStore.WebUI {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.unobtrusive*",
@@ -32,10 +32,10 @@ namespace SportsStore.WebUI {
                 "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+                "~/Scripts/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                 "~/Content/bootstrap.css",
+                 "~/Content/bootstrap.min.css",
                  "~/Content/Site.css"));
         }
     }
--- SportsStore/SportsStore.WebUI/Controllers/HomeController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/HomeController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SportsStore.WebUI.Controllers {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller {
         public ActionResult Index() {
             return View();
