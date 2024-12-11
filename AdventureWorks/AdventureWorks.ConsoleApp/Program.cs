8b9cba411e1e0b576761a9924fa32ea103bc31c1
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-12-11 08:41:39 -0500

    Changes found. CI to remote

--- AdventureWorks/AdventureWorks.ConsoleApp/Program.cs
+++ AdventureWorks/AdventureWorks.ConsoleApp/Program.cs
@@ -1,32 +1,29 @@
 ﻿using AdventureWorks.Domain.DataAccessLayer;
-using AdventureWorks.DomainRepository;
 using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.Design;
 using static System.Console;
 
+namespace AdventureWorks.ConsoleApp;
 
-namespace AdventureWorks.ConsoleApp {
-    internal class Program {
-        static void Main(string[] args) {
+internal class Program {
+    static void Main(string[] args) {
 
-            WriteLine("Welcome to Adventure works");
-            ConnectionGetDepartments();
-            ReadLine();
-        }
+        WriteLine("Welcome to Adventure works");
+        ConnectionGetDepartments();
+        ReadLine();
+    }
 
-        private static void ConnectionGetDepartments() {
-            //This connection queries the database just fine
+    private static void ConnectionGetDepartments() {
+        //This connection queries the database just fine
 
-            var optionsBuilder = new DbContextOptionsBuilder<AdWDbContext>();
-            optionsBuilder.UseSqlServer("Data Source=JEANPC\\DEVSERVER;Initial Catalog=AdventureWorks2022;Integrated Security=True;Encrypt=True");
+        var optionsBuilder = new DbContextOptionsBuilder<AdWDbContext>();
+        optionsBuilder.UseSqlServer("Data Source=JEANPC\\DEVSERVER;Initial Catalog=AdventureWorks2022;Integrated Security=True;Encrypt=True");
+        //The plan is move this to the appsettings.json file.
 
-            using (var context = new AdWDbContext(optionsBuilder.Options)) {
-                var departments = context.Departments.ToList();
+        using var context = new AdWDbContext(optionsBuilder.Options);
+        var departments = context.Departments.ToList();
 
-                foreach (var dept in departments) {
-                    WriteLine($"Id: {dept.DepartmentId}, Name: {dept.Name}, Price: {dept.GroupName}");
-                }
-            }
+        foreach (var dept in departments) {
+            WriteLine($"Id: {dept.DepartmentId}, Name: {dept.Name}, Price: {dept.GroupName}");
         }
     }
 }
