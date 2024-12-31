073509decd2927366b2d40069e3ec36b07c9dd45
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-12-31 05:36:14 -0500

    changes in progress

---
+++ DemoExercises/DEP.ServiceApplication/IPatientService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace DEP.ServiceApplication {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IPatientService {
+        [OperationContract, WebGet]
+        List<PatientInfo> GetAllPatientInfo();
+
+        [OperationContract, WebGet]
+        PatientInfo GetPatientInfo(int id);
+    }
+}
---
+++ DemoExercises/DEP.ServiceApplication/PatientService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DEP.ServiceApplication {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class PatientService : IPatientService {
+        public List<PatientInfo> GetAllPatientInfo() {
+
+            using (var db = new DEPEntities()) {
+                var patientInfo = db.PatientInfoes.ToList();
+                return patientInfo;
+            }
+        }
+
+        public PatientInfo GetPatientInfo(int id) {
+            using(var db = new DEPEntities()) {
+                var patientInfo = db.PatientInfoes.FirstOrDefault(p => p.PatientId == id);
+                return patientInfo;
+            }
+        }
+    }
+}
