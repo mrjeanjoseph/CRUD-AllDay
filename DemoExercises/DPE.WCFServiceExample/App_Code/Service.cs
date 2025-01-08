948f25d884bc0dbeaa22f811ea60720a08786bbf
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2025-01-08 17:10:05 -0500

    work in progress

---
+++ DemoExercises/DPE.WCFServiceExample/App_Code/Service.cs
@@ -0,0 +1,8 @@
+﻿// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService {
+    public string GetData(int value) {
+
+        return string.Format("You entered: {0}", value);
+
+    }
+}
