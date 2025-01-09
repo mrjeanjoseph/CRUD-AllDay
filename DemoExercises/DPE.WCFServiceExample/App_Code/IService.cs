48233e2ea6a5bcf878136ae16190ca564001ae82
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2025-01-08 19:40:41 -0500

    work in progress

--- DemoExercises/DPE.WCFServiceExample/App_Code/IService.cs
+++ DemoExercises/DPE.WCFServiceExample/App_Code/IService.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
 using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
 
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
 [ServiceContract]
 public interface IService
 {
 
-	[OperationContract]
-	string GetData(int value);
+    [OperationContract]
+    string GetData(int value);
 
-	// TODO: Add your service operations here
+    [OperationContract]
+    List<int> GetAllData(int value); // Or you can use int[] if you prefer an array
+
+    // TODO: Add your service operations here
 }
