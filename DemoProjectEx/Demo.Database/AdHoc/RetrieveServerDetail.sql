135674272cc6ef3369f66f0c82aac20e58758780
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2025-07-30 21:10:42 -0400

    additinal cleanup
    added a common script

---
+++ DemoProjectEx/Demo.Database/AdHoc/RetrieveServerDetail.sql
@@ -0,0 +1,23 @@
+﻿-- Script to retrieve server details
+SELECT 
+    SERVERPROPERTY('MachineName') AS MachineName,
+    SERVERPROPERTY('ServerName') AS ServerName,
+    SERVERPROPERTY('Edition') AS Edition,
+    SERVERPROPERTY('ProductVersion') AS ProductVersion,
+    SERVERPROPERTY('ProductLevel') AS ProductLevel,
+    SERVERPROPERTY('EngineEdition') AS EngineEdition,
+    SERVERPROPERTY('IsClustered') AS IsClustered,
+    SERVERPROPERTY('Collation') AS Collation;
+
+
+
+DROP DATABASE IF EXISTS [MySimpleDatabase];
+CREATE DATABASE MySimpleDatabase;
+GO
+
+USE [master];
+GO
+ALTER DATABASE [MySimpleDatabase] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
+DROP DATABASE [MySimpleDatabase];
+GO
